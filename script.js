4fabe7565d0ae178583235f89c6fddd34f73b40a
Sourabh Banik <81009734+Sourabh250@users.noreply.github.com>
2024-01-22 02:17:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ async function dynamic() {
         if (data) {
             data.forEach(element => {
                 $('#main-container').append(`
-                <div class="flex flex-col items-center p-2 shadow-lg h-[600px]">
+                <div class="flex flex-col items-center p-2 shadow-lg h-[600px] text-base md:text-md">
                 <img src="${element.image}" class="w-1/2 md:w-1/4 h-1/2" alt="Image Of Product">
                 <div class="px-6 py-4 flex flex-col h-1/2">
-                <h1 class="font-bold text-xl mb-2 text-stone-700">${element.title}</h1>
+                <h1 class="font-bold text-lg md:text-xl mb-2 text-stone-700">${element.title}</h1>
                 <p class="text-gray-700 overflow-hidden">${element.description}</p>
-                <div class="pt-4 pb-2 text-gray-700 mt-auto overflow-hidden"><span class="inline-block bg-gray-300 rounded-full px-3 my-3 font-semibold hover:bg-sky-500 cursor-pointer hover:text-white">Category: ${element.category}</span>
+                <div class="pt-4 pb-2 text-gray-700 mt-auto "><span class="inline-block bg-gray-300 rounded-full px-3 my-3 font-semibold hover:bg-sky-500 cursor-pointer hover:text-white">Category: ${element.category}</span>
                 <span class="inline-block bg-gray-300 rounded-full my-3 px-3 font-semibold hover:bg-sky-500 cursor-pointer hover:text-white">Price: ${element.price}</span>
                 <span class="inline-block bg-gray-300 rounded-full my-3  px-3 font-semibold hover:bg-sky-500 cursor-pointer hover:text-white">Rating: ${element.rating.rate}</span>
                 <span class="inline-block bg-gray-300 rounded-full my-3 px-3 font-semibold hover:bg-sky-500 cursor-pointer hover:text-white">Count: ${element.rating.count}</span></div>
